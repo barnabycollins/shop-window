fcb5df9b2eea86bd94cd87711ee5e18033b87aed
Barnaby Collins <barnaby@barnabycollins.com>
2023-12-09 15:51:48 +0000

    Work on app

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
-import React from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { drive } from "@googleapis/drive";
 import { getQueryParams } from "./getQueryParams";
+import { BarLoader } from "react-spinners";
 
 const REQUIRED_PARAMS = ["googleApiKey", "driveFolderId"] as const;
 const OPTIONAL_PARAMS = ["rotation"] as const;
@@ -12,25 +13,43 @@ const { params, missingParams } = getQueryParams(
 );
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const client = drive({ version: "v3", auth: params.googleApiKey });
+    const getStuff = async () => {
+      const response = await client.files.list({
+        q: `'${params.driveFolderId}' in parents and trashed = false`,
+      });
+      console.log(response);
+    };
+
+    getStuff();
+  }, []);
+
   return missingParams.length > 0 ? (
     <div className="error-msg">
-      <div>
-        <h1>Shop Window App: Error</h1>
-        <p>The following parameters are missing from the URL:</p>
-        <ul>
-          {missingParams.map((name) => (
-            <li>{name}</li>
-          ))}
-        </ul>
-      </div>
+      <h1>Shop Window App: Error</h1>
+      <p>The following parameter names are missing from the URL:</p>
+      <ul className="mono">
+        {missingParams.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <p>
+        Please add them again, in the format:{" "}
+        <span className="mono">path.to/page?name1=value1&name2=value2</span>
+      </p>
     </div>
+  ) : isLoaded ? (
+    <h1>yes!</h1>
   ) : (
-    <h1>happy now</h1>
+    <BarLoader color="#ffffff" />
   );
 }
 
-ReactDOM.createRoot(document.getElementById("slideshow-container")).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("slideshow-container")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
