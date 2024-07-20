fe0a8d7276f63865cfc4b6203743ce4a298cb184
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2024-07-20 09:48:29 +0100

    Finish work on JSON stuff

--- src/getParams.ts
+++ src/getParams.ts
@@ -3,107 +3,150 @@ import {
   getGoogleDriveDownloadUrl,
   GOOGLE_DRIVE_EXPANDED_FILE_FIELDS,
 } from "./googleDriveConstants";
+import { Assert, ExactProps, Int, ThrowError } from "ts-runtime-checks";
 
-export const REQUIRED_PARAMS = ["googleApiKey", "driveFolderId"] as const;
-export const OPTIONAL_PARAMS = [
+const REQUIRED_PARAMS = ["googleApiKey", "driveFolderId"] as const;
+type RequiredParam = (typeof REQUIRED_PARAMS)[number];
+
+const OPTIONAL_PARAMS = [
   "rotation",
   "slideLength",
   "sharedDriveId",
   "animate",
   "refetchInterval",
+  "enabledMimeTypes",
 ] as const;
+type OptionalParam = (typeof OPTIONAL_PARAMS)[number];
 
-const supportedMimeTypes = [
-  "image/avif",
-  "image/gif",
-  "image/jpeg",
-  "image/png",
-  "image/svg+xml",
-  "image/webp",
+const NUMERIC_PARAMS: (RequiredParam | OptionalParam)[] = [
+  "rotation",
+  "slideLength",
+  "refetchInterval",
 ];
 
+const BOOLEAN_PARAMS: (RequiredParam | OptionalParam)[] = ["animate"];
+
+type JsonParamSchema = {
+  rotation?: 0 | 90 | 180 | 270;
+  slideLength?: Int;
+  animate?: boolean;
+  refetchInterval?: Int;
+  enabledMimeTypes?: string[];
+};
+
+type UrlParamsSchema = JsonParamSchema & {
+  googleApiKey: string;
+  driveFolderId: string;
+  sharedDriveId?: string;
+};
+
 export type AppConfig = {
-  imageUrls: string[];
   rotation: 0 | 90 | 180 | 270;
   slideLength: number;
   animate: boolean;
   refetchInterval?: number;
+  enabledMimeTypes: string[];
 };
 
-function getQueryParams<R extends string, O extends string>(
-  required: readonly R[],
-  optional: readonly O[]
-) {
-  type Return = {
-    params: Record<R, string> & Partial<Record<O, string>>;
-    missingParams: R[];
-  };
+type AppConfigWithMediaUrls = AppConfig & {
+  mediaUrls: string[];
+};
 
-  const params: Partial<Record<R | O, string>> = {};
+const defaultConfig: AppConfig = {
+  slideLength: 30,
+  animate: true,
+  rotation: 0,
+  enabledMimeTypes: [
+    "image/avif",
+    "image/gif",
+    "image/jpeg",
+    "image/png",
+    "image/svg+xml",
+    "image/webp",
+  ],
+};
 
+export class ParamValidationError extends Error {
+  constructor(message: any) {
+    super(message);
+    this.name = "ParamValidationError";
+  }
+}
+
+function parseBoolean(input: string) {
+  if (input === "true") return true;
+  if (input === "false") return false;
+  return null;
+}
+
+function getQueryParams(): UrlParamsSchema {
   const urlSearch = new URLSearchParams(window.location.search);
+  let receivedParams: { [k: string]: string } = {};
 
   const missingParams: string[] = [];
 
-  required.forEach((name) => {
+  REQUIRED_PARAMS.forEach((name) => {
     const value = urlSearch.get(name);
     if (!value) missingParams.push(name);
-    else params[name] = value;
+    else receivedParams[name] = value;
   });
 
-  optional.forEach((name) => {
+  if (missingParams.length > 0) {
+    throw new ParamValidationError(missingParams);
+  }
+
+  OPTIONAL_PARAMS.forEach((name) => {
     const value = urlSearch.get(name);
-    if (value) params[name] = value;
+    if (value) receivedParams[name] = value;
   });
 
-  console.debug(`Received params:`, params);
+  const finalParams = Object.fromEntries(
+    Object.entries(receivedParams)
+      .map(([key, value]) => {
+        if (key in NUMERIC_PARAMS)
+          return [key, Number(value)] as [string, number];
+        if (key in BOOLEAN_PARAMS)
+          return [key, parseBoolean(value)] as [string, boolean | null];
+        return [key, value] as [string, string];
+      })
+      .filter(
+        (entry) =>
+          !(
+            [null, undefined] as (
+              | string
+              | number
+              | boolean
+              | null
+              | undefined
+            )[]
+          ).includes(entry[1])
+      )
+  ) as Assert<
+    ExactProps<UrlParamsSchema, true, true>,
+    ThrowError<ParamValidationError>
+  >;
 
-  return { params, missingParams } as Return;
+  return finalParams;
 }
 
-export async function getConfig(): AppConfig {
-  const { params, missingParams } = getQueryParams(
-    REQUIRED_PARAMS,
-    OPTIONAL_PARAMS
-  );
+export async function getConfig(): Promise<AppConfigWithMediaUrls> {
+  const urlParamsConfig = getQueryParams();
 
   const apiUrl =
     `https://www.googleapis.com/drive/v3/files?` +
     new URLSearchParams({
-      key: params.googleApiKey,
-      q: `'${params.driveFolderId}' in parents and trashed = false`,
+      key: urlParamsConfig.googleApiKey,
+      q: `'${urlParamsConfig.driveFolderId}' in parents and trashed = false`,
       fields: `files(${GOOGLE_DRIVE_EXPANDED_FILE_FIELDS.join(",")})`,
-      ...(params.sharedDriveId
+      ...(urlParamsConfig.sharedDriveId
         ? {
             includeItemsFromAllDrives: "true",
             supportsAllDrives: "true",
-            driveId: params.sharedDriveId,
+            driveId: urlParamsConfig.sharedDriveId,
             corpora: "drive",
           }
         : {}),
     });
-
-  const slideLength =
-    (params.slideLength ? Math.max(parseInt(params.slideLength, 10), 1) : 30) *
-    1000;
-
-  const refetchInterval = params.refetchInterval
-    ? parseInt(params.refetchInterval, 10) * 60 * 1000
-    : undefined;
-
-  const animate = params.animate === "false" ? false : true;
-
-  const rotationMap: { [key: string]: 90 | 180 | 270 } = {
-    "90": 90,
-    "180": 180,
-    "270": 270,
-    "-90": 270,
-  };
-
-  const rotation =
-    params.rotation && params.rotation in rotationMap
-      ? rotationMap[params.rotation]
-      : 0;
 
   const response = await fetch(apiUrl);
   const imageBody = (await response.json()) as FilesListResponse;
@@ -112,25 +155,59 @@ export async function getConfig(): AppConfig {
     (f) => f.mimeType === "application/json"
   );
 
-  let mimeTypes = supportedMimeTypes;
+  let jsonConfig: JsonParamSchema = {};
 
   if (jsonFiles.length > 0) {
     for (const file of jsonFiles) {
-      const fileResponse = await fetch(getGoogleDriveDownloadUrl(file.id));
+      let fileResponse: Response;
+      let fileContent: JsonParamSchema = {};
 
-      const jsonContent = await response.json();
+      try {
+        fileResponse = await fetch(getGoogleDriveDownloadUrl(file.id));
+      } catch (e) {
+        console.error(
+          `Failed to retrieve JSON configuration file with ID ${file.id}:`,
+          e
+        );
+      }
+      try {
+        fileContent = await response.json();
+      } catch (e) {
+        console.error(`Failed to parse JSON file!`);
+      }
 
-      // TODO handle JSON config
+      try {
+        fileContent = fileContent as Assert<
+          ExactProps<JsonParamSchema, true, true>
+        >;
+      } catch (e) {
+        console.error(
+          `Found invalid value(s) in JSON file with ID ${file.id}:`,
+          e
+        );
+      }
+
+      jsonConfig = {
+        ...jsonConfig,
+        ...fileContent,
+      };
     }
   }
 
+  const resolvedConfig: AppConfig = {
+    ...defaultConfig,
+    ...urlParamsConfig,
+    ...jsonConfig,
+  };
+
+  resolvedConfig.slideLength *= 1000;
+  if (resolvedConfig.refetchInterval)
+    resolvedConfig.refetchInterval *= 60 * 1000;
+
   return {
-    imageUrls: imageBody.files
-      .filter((f) => mimeTypes.includes(f.mimeType))
+    ...resolvedConfig,
+    mediaUrls: imageBody.files
+      .filter((f) => resolvedConfig.enabledMimeTypes.includes(f.mimeType))
       .map((f) => getGoogleDriveDownloadUrl(f.id)),
-    slideLength,
-    refetchInterval,
-    animate,
-    rotation,
   };
 }
